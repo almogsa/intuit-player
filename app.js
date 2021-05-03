1b424d26e858ac1a3f768b842fce89d924b2d088
Almog <almogsa@gmail.com>
2021-05-03 12:13:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+
+const NEW_SONG_EVENT = "newSongEvent";
+const UPDATE_PLAYLIST_EVENT = "updatePlaylistEvent";
+const HISTORY_PLAYLIST_EVENT = "historyPlaylistEvent";
+const UPDATE_SONG_EVENT = "updateSongEvent";
+const GET_HISTORY_EVENT = "getHistoryEvent";
+const LOGIN_EVENT = "login";
+
+let history = [];
+io.on("connection", (socket) => {
+    socket.on(LOGIN_EVENT, () => {
+        console.log(`Client ${socket.id} connected`);
+        socket.join();
+        io.emit('ready', history);
+    })
+    // Listen for new song event
+    socket.on(NEW_SONG_EVENT, (song) => {
+        history.push(song);
+        io.emit(NEW_SONG_EVENT, song);
+    });
+    // get initial data
+    socket.on('GET_HISTORY', () => {
+        io.emit(HISTORY_PLAYLIST_EVENT, history);
+    });
+
+    // Listen for update playlist event
+    socket.on(UPDATE_PLAYLIST_EVENT, (song) => {
+        history = history.filter(item => item.id !== song.id);
+        io.emit(UPDATE_PLAYLIST_EVENT, history);
+    });
+    //  closes the socket
+    socket.on("disconnect", () => {
+        console.log(`Client ${socket.id} diconnected`);
+        socket.leave();
+    });
+});
+http.listen(port, () => console.log(`API listening on ${port}`));
