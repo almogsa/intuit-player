6dae615a824bd5ac699a8b73eef57b8d2d5d7d35
Almog <almogsa@gmail.com>
2021-06-19 16:57:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const HISTORY_PLAYLIST_EVENT = "historyPlaylistEvent";
 const LOGIN_EVENT = "login";
 const GET_HISTORY_EVENT = "getHistoryEvent";
 
-let history = [];
+let history = [
+    'https://www.youtube.com/watch?v=kiqEEr7CK2g',
+    'https://www.youtube.com/watch?v=AzfZ7hLR17s',
+    'https://www.youtube.com/watch?v=tO7CCP7liwI&t=1454s',
+    'https://www.youtube.com/watch?v=0sMRC1l-INs',
+    'https://www.youtube.com/watch?v=Qk2zGLMmp0s',
+    'https://www.youtube.com/watch?v=ZPCaPOnFoWM',
+
+];
 io.on("connection", (socket) => {
     socket.on(LOGIN_EVENT, () => {
         console.log(`Client ${socket.id} connected`);
